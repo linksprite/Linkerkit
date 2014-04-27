67dc2bdd6ae8f73d48a7646a2b5f94017080e1b1
linksprite <support@linksprite.com>
2014-04-26 20:54:19 -0600

    first release

---
+++ Linker_4D7SLED/examples/NumberFlow/NumberFlow.ino
@@ -0,0 +1,34 @@
+#include "TM1637.h"
+#define CLK 3//pins definitions for TM1637 and can be changed to other ports       
+#define DIO 2
+TM1637 tm1637(CLK,DIO);
+void setup()
+{
+  tm1637.init();
+  tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+}
+void loop()
+{
+  int8_t NumTab[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};//0~9,A,b,C,d,E,F
+  int8_t ListDisp[4];
+  unsigned char i = 0;
+  unsigned char count = 0;
+  delay(150);
+  while(1)
+  {
+    i = count;
+    count ++;
+    if(count == sizeof(NumTab)) count = 0;
+    for(unsigned char BitSelect = 0;BitSelect < 4;BitSelect ++)
+    {
+      ListDisp[BitSelect] = NumTab[i];
+      i ++;
+      if(i == sizeof(NumTab)) i = 0;
+    }
+    tm1637.display(0,ListDisp[0]);
+    tm1637.display(1,ListDisp[1]); 
+    tm1637.display(2,ListDisp[2]);
+    tm1637.display(3,ListDisp[3]);
+    delay(300);
+  }
+}
